5c8079e6d817789d3262ae1538f04241bfe04b63
Wffv9FNa <github_general_719@mcilvaney.co.uk>
2025-04-20 23:00:54 +0100

    feat(init): initial QMK Moonlander keyboard setup
    - Add keyboard configuration files (config.h, rules.mk)
    - Add keymap configuration (keymap.c, keymap.json)
    - Add i18n support
    - Add custom song list feature
    - Configure git submodules

---
+++ config.h
@@ -0,0 +1,31 @@
+/* Copyright 2020 ZSA Technology Labs, Inc <@zsa>
+ * Copyright 2020 Jack Humbert <jack.humb@gmail.com>
+ * Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define ORYX_CONFIGURATOR
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define CAPS_LOCK_STATUS
+#define SERIAL_NUMBER "v9D0K/VqyORY"
+#define LAYER_STATE_8BIT
+
+#define RGB_MATRIX_STARTUP_SPD 60
+
+#ifdef AUDIO_ENABLE
+# define STARTUP_SONG SONG(CLOSE_ENCOUNTERS_5_NOTE)
+#endif
---
+++ i18n.h
@@ -0,0 +1,6 @@
+#define UK_ASTR S(KC_8)
+#define UK_PLUS S(KC_EQL)
+#define UK_SLSH KC_SLSH
+#define UK_MINS KC_MINS
+#define UK_DOT KC_DOT
+#define UK_EQL KC_EQL
