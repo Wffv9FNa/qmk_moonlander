d9afdac6445d74a1bccdde77af4143f218a7b906
Wffv9FNa <github_general_719@mcilvaney.co.uk>
2025-04-21 01:38:28 +0100

    refactor(keymap): format keymap layout for improved readability

--- keymap.c
+++ keymap.c
@@ -46,39 +46,39 @@ socd_cleaner_t socd_opposing_pairs[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_EQL,         KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           TG(1),                        TG(1),          KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINS,
-        KC_DEL,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           TG(2),                        TG(2),          KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,
-        KC_BSPC,        KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_NUBS,                      KC_MEH,         KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        MT(MOD_LGUI, KC_QUOTE),
-        KC_LEFT_SHIFT,  MT(MOD_LCTL, KC_Z),KC_X,       KC_C,           KC_V,           KC_B,                                          KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,        KC_RIGHT_SHIFT,
-        MO(2),          KC_LEFT_GUI,    CW_TOGG,        KC_TRNS,        MO(1),          KC_CAPS,                                            KC_ESC,         MO(3),          KC_HYPR,        KC_LBRC,        KC_RBRC,        MO(1),
-                        KC_SPC,         KC_LEFT_ALT,    KC_F13,                         KC_BSPC,        KC_TAB,             KC_ENT
-    ),
+KC_EQL,         	KC_1,           		KC_2,           KC_3,           KC_4,           KC_5,           TG(1),                        TG(1),          KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINS,
+KC_DEL,         	KC_Q,           		KC_W,           KC_E,           KC_R,           KC_T,           TG(2),                        TG(2),          KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,
+KC_BSPC,        	KC_A,           		KC_S,           KC_D,           KC_F,           KC_G,           KC_NUBS,                      KC_MEH,         KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        MT(MOD_LGUI, KC_QUOTE),
+KC_LEFT_SHIFT,  	MT(MOD_LCTL, KC_Z),	KC_X,       	  KC_C,           KC_V,           KC_B,                                         				        KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,        KC_RIGHT_SHIFT,
+MO(2),          	KC_LEFT_GUI,    		CW_TOGG,        KC_TRNS,        MO(1),          						KC_CAPS,  KC_ESC,     								                            MO(3),          KC_HYPR,        KC_LBRC,        KC_RBRC,        MO(1),
+																								                              KC_SPC, KC_LEFT_ALT, KC_F13,  KC_BSPC, KC_TAB, KC_ENT
+),
 
     [1] = LAYOUT(
-        KC_GRV,         KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_TRNS,            KC_TRNS,        KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
-        KC_TRNS,        KC_NO,          LCTL(LSFT(KC_LEFT)),KC_NO,      LCTL(LSFT(KC_RIGHT)),KC_NUBS,   KC_NO,              KC_NO,          KC_HOME,        KC_7,           KC_8,           KC_9,           KC_PAST,        KC_F12,
-        KC_TRNS,        KC_NO,          LGUI(LCTL(KC_Y)),KC_NO,         LCTL(LSFT(KC_X)),KC_NO,         KC_TRNS,            KC_NO,          KC_END,         KC_4,           KC_5,           KC_6,           KC_PPLS,        KC_NO,
-        KC_TRNS,        KC_LEFT_CTRL,   KC_NO,          KC_NO,          KC_NO,          KC_NO,                                              KC_PSLS,        KC_1,           KC_2,           KC_3,           KC_PMNS,        KC_TRNS,
-        KC_NO,          KC_TRNS,        KC_NO,          KC_NO,          KC_TRNS,        KC_NO,                                              KC_TRNS,        KC_NO,          KC_0,           KC_PDOT,        KC_PEQL,        KC_TRNS,
-                        KC_TRNS,        KC_NO,          KC_NO,                          KC_TRNS,        KC_TRNS,            KC_TRNS
+        KC_GRV,         KC_F1,          KC_F2,          		  KC_F3,          KC_F4,          		    KC_F5,          KC_TRNS,        			      KC_TRNS,        KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
+        KC_TRNS,        KC_NO,          LCTL(LSFT(KC_LEFT)),	KC_NO,      	  LCTL(LSFT(KC_RIGHT)),	  KC_NUBS,   		  KC_NO,          			      KC_NO,          KC_HOME,        KC_7,           KC_8,           KC_9,           KC_PAST,        KC_F12,
+        KC_TRNS,        KC_NO,          LGUI(LCTL(KC_Y)),		  KC_NO,         	LCTL(LSFT(KC_X)),		    KC_NO,         	KC_TRNS,        			      KC_NO,          KC_END,         KC_4,           KC_5,           KC_6,           KC_PPLS,        KC_NO,
+        KC_TRNS,        KC_LEFT_CTRL,   KC_NO,          		  KC_NO,          KC_NO,          		    KC_NO,                          			                      KC_PSLS,        KC_1,           KC_2,           KC_3,           KC_PMNS,        KC_TRNS,
+        KC_NO,          KC_TRNS,        KC_NO,          		  KC_NO,          KC_TRNS,        									                    KC_NO,	  KC_TRNS,                            KC_NO,          KC_0,           KC_PDOT,        KC_PEQL,        KC_TRNS,
+                                                                                                                    KC_TRNS, KC_NO, KC_NO,  	KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
     [2] = LAYOUT(
-        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,              KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
-        KC_TRNS,        LCTL(KC_Q),     LCTL(KC_W),     LCTL(KC_E),     LCTL(KC_R),     KC_NO,          KC_TRNS,            KC_TRNS,        KC_HOME,        KC_NO,          KC_UP,          KC_NO,          KC_NO,          KC_NO,
-        KC_TRNS,        LCTL(KC_A),     LCTL(KC_S),     KC_NO,          LCTL(KC_F),     KC_NO,          KC_NO,              KC_NO,          KC_END,         KC_LEFT,        KC_DOWN,        KC_RGHT,        KC_NO,          KC_NO,
-        KC_TRNS,        LCTL(KC_Z),     LCTL(KC_X),     LCTL(KC_C),     LCTL(KC_V),     LCTL(KC_B),                                         KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_TRNS,
-        KC_TRNS,        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                              KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_TRNS,
-                        KC_NO,          KC_NO,          KC_TRNS,                        KC_NO,          KC_TRNS,            KC_NO
+        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,              			            KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+        KC_TRNS,        LCTL(KC_Q),     LCTL(KC_W),     LCTL(KC_E),     LCTL(KC_R),     KC_NO,          KC_TRNS,            			            KC_TRNS,        KC_HOME,        KC_NO,          KC_UP,          KC_NO,          KC_NO,          KC_NO,
+        KC_TRNS,        LCTL(KC_A),     LCTL(KC_S),     KC_NO,          LCTL(KC_F),     KC_NO,          KC_NO,              			            KC_NO,          KC_END,         KC_LEFT,        KC_DOWN,        KC_RGHT,        KC_NO,          KC_NO,
+        KC_TRNS,        LCTL(KC_Z),     LCTL(KC_X),     LCTL(KC_C),     LCTL(KC_V),     LCTL(KC_B),                         			                            KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_TRNS,
+        KC_TRNS,        KC_NO,          KC_NO,          KC_NO,          KC_NO,          								                    KC_NO,      KC_NO,          							        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_TRNS,
+																					                                                                  KC_NO, KC_NO, KC_TRNS,     	KC_NO, KC_TRNS, KC_NO
     ),
 
     [3] = LAYOUT(
-        QK_BOOT,        KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_NO,              KC_NO,          KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
-        KC_NO,          KC_NO,          LSFT(KC_LEFT),  LCTL(LSFT(KC_X)),LSFT(KC_RGHT), KC_NO,          KC_NO,              KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_F12,
-        KC_NO,          LCTL(KC_BSPC),  LCTL(KC_LEFT),  LGUI(LCTL(KC_Y)),LCTL(KC_RGHT), KC_NO,          KC_NO,              KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
-        KC_NO,          LCTL(LSFT(KC_BSPC)),LCTL(LSFT(KC_LEFT)),KC_NO,  LCTL(LSFT(KC_RIGHT)),KC_NO,                                         KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
-        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                              KC_NO,          KC_TRNS,        SOCDTOG,        KC_NO,          KC_NO,          KC_NO,
-                        KC_NO,          KC_NO,          KC_NO,                          KC_NO,          KC_NO,              KC_NO
+        QK_BOOT,        KC_F1,          		  KC_F2,          		    KC_F3,          	    KC_F4,          		    KC_F5,          KC_NO,         							KC_NO,          KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
+        KC_NO,          KC_NO,          		  LSFT(KC_LEFT),  		    LCTL(LSFT(KC_X)),	    LSFT(KC_RGHT), 			    KC_NO,          KC_NO,         							KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_F12,
+        KC_NO,          LCTL(KC_BSPC),  		  LCTL(KC_LEFT),  		    LGUI(LCTL(KC_Y)),	    LCTL(KC_RGHT), 			    KC_NO,          KC_NO,         							KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+        KC_NO,          LCTL(LSFT(KC_BSPC)),	LCTL(LSFT(KC_LEFT)),	  KC_NO,  			        LCTL(LSFT(KC_RIGHT)),	  KC_NO,                    							                	  KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+        KC_NO,          KC_NO,          		  KC_NO,          		    KC_NO,          	    KC_NO,          									              KC_NO,		 KC_NO,											              KC_TRNS,        SOCDTOG,        KC_NO,          KC_NO,          KC_NO,
+																														                                                                KC_NO, KC_NO, KC_NO, 					KC_NO, KC_NO, KC_NO
     )
 };
 
