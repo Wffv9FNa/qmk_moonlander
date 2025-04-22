a82a65ebe37c7106374d0124afa66b5dbcbfc3fb
Wffv9FNa <github_general_719@mcilvaney.co.uk>
2025-04-22 21:12:37 +0100

    refactor(keymap): improve keymap layout and add clang-format directives
    - Add clang-format directives to keymap.c for consistent code formatting
    - Update keymap layout by adding function keys (F5, F11, F12) and modifying existing key assignments
    - Enhance organization and readability of the keymap structure

--- keymap.c
+++ keymap.c
@@ -72,6 +72,7 @@ socd_cleaner_t socd_opposing_pairs[] = {
 // +---------+
 // | KEYMAPS |
 // +---------+
+//clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
 KC_EQL,         	KC_1,           		KC_2,           KC_3,           KC_4,           KC_5,           TG(1),                        TG(1),          KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINS,
@@ -109,14 +110,15 @@ MO(2),          	KC_LEFT_GUI,    		CW_TOGG,        TD(TD_TGLL_4),  MO(1),       
 																														                                                                KC_NO, KC_NO, KC_NO, 					KC_NO, KC_NO, KC_NO
     ),
     [4]=LAYOUT(
-      KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_NO,                              KC_NO,      KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,
-      KC_NO,          KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_NO,                              KC_NO,      KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,
-      KC_NO,          KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_NO,                              KC_NO,      KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,
-      KC_LEFT_SHIFT,  KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                                           KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,
-      KC_LEFT_CTRL,   KC_NO,          KC_NO,          TD(TD_TGLL_4),  KC_NO,                                  KC_ESCAPE,      KC_CAPS,                                KC_NO,          KC_NO,          KC_TRNS,        KC_TRNS,        KC_NO,
+      KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_F5,                              KC_TRNS,      KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_F11,
+      KC_NO,          KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_NO,                              KC_HOME,      KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_F12,
+      KC_NO,          KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_NO,                              KC_END,       KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,
+      KC_LEFT_SHIFT,  KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                                             KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,
+      KC_LEFT_CTRL,   KC_NO,          KC_NO,          TD(TD_TGLL_4),  KC_NO,                                  KC_ESCAPE,      KC_CAPS,                                  KC_NO,          KC_NO,          KC_TRNS,        KC_TRNS,        KC_NO,
                                                                                       KC_SPACE,   KC_TAB,    KC_LEFT_ALT,        KC_NO,   KC_NO,  KC_NO
 ),
 };
+//clang-format on
 
 // +----------------+
 // | USER FUNCTIONS |
