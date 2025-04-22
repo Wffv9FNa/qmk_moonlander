0d994d5ab06910d6e7aa6bda2b8737615ac8060e
Wffv9FNa <github_general_719@mcilvaney.co.uk>
2025-04-22 15:47:52 +0100

    chore(keymap): update keymap and configuration files
    - Add new custom keycode for selecting words in keymap.c
    - Update keymap.json to include the new select_word module
    - Exclude builds directory in .gitignore to prevent tracking of build artifacts
    - Remove obsolete binary file zsa_moonlander_mykeys.bin

--- keymap.c
+++ keymap.c
@@ -84,7 +84,7 @@ MO(2),          	KC_LEFT_GUI,    		CW_TOGG,        TD(TD_TGLL_4),  MO(1),       
 
     [1] = LAYOUT(
         KC_GRV,         KC_F1,          KC_F2,          		  KC_F3,          KC_F4,          		    KC_F5,          KC_TRNS,        			      KC_TRNS,        KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
-        KC_TRNS,        KC_NO,          LCTL(LSFT(KC_LEFT)),	KC_NO,      	  LCTL(LSFT(KC_RIGHT)),	  KC_NUBS,   		  KC_NO,          			      KC_NO,          KC_HOME,        KC_7,           KC_8,           KC_9,           KC_PAST,        KC_F12,
+        KC_TRNS,        KC_NO,          SELWBAK,              KC_NO,      	  SELWORD,                KC_NUBS,        KC_NO,          			      KC_NO,          KC_HOME,        KC_7,           KC_8,           KC_9,           KC_PAST,        KC_F12,
         KC_TRNS,        KC_NO,          LGUI(LCTL(KC_Y)),		  KC_NO,         	LCTL(LSFT(KC_X)),		    KC_NO,         	KC_TRNS,        			      KC_NO,          KC_END,         KC_4,           KC_5,           KC_6,           KC_PPLS,        KC_NO,
         KC_TRNS,        KC_LEFT_CTRL,   KC_NO,          		  KC_NO,          KC_NO,          		    KC_NO,                          			                      KC_PSLS,        KC_1,           KC_2,           KC_3,           KC_PMNS,        KC_TRNS,
         KC_NO,          KC_TRNS,        KC_NO,          		  KC_NO,          KC_TRNS,        									                    KC_NO,	  KC_TRNS,                            KC_NO,          KC_0,           KC_PDOT,        KC_PEQL,        KC_TRNS,
