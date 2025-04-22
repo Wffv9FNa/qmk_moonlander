6d515d6b29697c8eb05bec20679a5599cdb62858
Wffv9FNa <github_general_719@mcilvaney.co.uk>
2025-04-23 00:20:29 +0100

    refactor(keymap): adjust keymap layout for improved readability
    - Reformat keymap.c to enhance alignment and clarity of key assignments
    - Maintain existing functionality while improving overall organization of the keymap structure

--- keymap.c
+++ keymap.c
@@ -75,11 +75,11 @@ socd_cleaner_t socd_opposing_pairs[] = {
 //clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-KC_EQL,         	KC_1,           		KC_2,           KC_3,           KC_4,           KC_5,           TG(1),                        TG(1),          KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINS,
-KC_DEL,         	KC_Q,           		KC_W,           KC_E,           KC_R,           KC_T,           TG(2),                        TG(2),          KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,
-KC_BSPC,        	KC_A,           		KC_S,           KC_D,           KC_F,           KC_G,           KC_NUBS,                      KC_MEH,         KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        MT(MOD_LGUI, KC_QUOTE),
-KC_LEFT_SHIFT,  	MT(MOD_LCTL, KC_Z),	KC_X,       	  KC_C,           KC_V,           KC_B,                                         				        KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,        KC_RIGHT_SHIFT,
-MO(2),          	KC_LEFT_GUI,    		CW_TOGG,        TD(TD_TGLL_4),  MO(1),          						KC_CAPS,  KC_ESC,     								                            MO(3),          KC_HYPR,        KC_LBRC,        KC_RBRC,        MO(1),
+        KC_EQL,         	KC_1,           		KC_2,           KC_3,           KC_4,           KC_5,           TG(1),                        TG(1),          KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINS,
+        KC_DEL,         	KC_Q,           		KC_W,           KC_E,           KC_R,           KC_T,           TG(2),                        TG(2),          KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,
+        KC_BSPC,        	KC_A,           		KC_S,           KC_D,           KC_F,           KC_G,           KC_NUBS,                      KC_MEH,         KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        MT(MOD_LGUI, KC_QUOTE),
+        KC_LEFT_SHIFT,  	MT(MOD_LCTL, KC_Z),	KC_X,       	  KC_C,           KC_V,           KC_B,                                         				        KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,        KC_RIGHT_SHIFT,
+        MO(2),          	KC_LEFT_GUI,    		CW_TOGG,        TD(TD_TGLL_4),  MO(1),          						KC_CAPS,  KC_ESC,     								                            MO(3),          KC_HYPR,        KC_LBRC,        KC_RBRC,        MO(1),
 																								                              KC_SPC, KC_LEFT_ALT, KC_F13,  KC_BSPC, KC_TAB, KC_ENT
 ),
 
