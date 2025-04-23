881afaa7d9416dcb3248676f5033561b5bd551cd
Wffv9FNa <github_general_719@mcilvaney.co.uk>
2025-04-23 18:00:40 +0100

    refactor(rgb_config): transition to HSV color model for LED mapping
    - Update the COLOR_LAYOUT macro to utilize HSV triplets instead of RGB, enhancing color representation.
    - Revise the ledmap_layout to reflect the new HSV structure, improving clarity and maintainability.
    - Modify the set_layer_color function to handle HSV values, ensuring proper color application and brightness adjustment.
    - Maintain existing functionality while improving the overall organization of the RGB configuration.

--- rgb_config/rgb_config.c
+++ rgb_config/rgb_config.c
@@ -3,7 +3,7 @@
 #include "rgb_config.h"
 #include "color_aliases.h"
 
-// Typedef for color triplet for readability
+// Typedef for color triplet for readability (represents HSV)
 typedef uint8_t ColorTriplet[3];
 
 /* RGB color aliases based on comments in color_aliases.h */
@@ -32,102 +32,139 @@ typedef uint8_t ColorTriplet[3];
 #define CFG_RGB_SPR_ISH CFG_RGB_SPGRN   /* { 74, 255, 206} */
 #define CFG_RGB_CYA_ISH CFG_RGB_CYPRO   /* {131, 255, 255} */
 
-// New layout-based LED map, defined using COLOR_LAYOUT and RGB aliases (Attempt 2)
+// New layout-based LED map, defined using COLOR_LAYOUT and HSV aliases
 static const uint8_t PROGMEM ledmap_layout[][MATRIX_ROWS][MATRIX_COLS][3] = {
+
+/*  _                             ___   */
+/* | |    __ _ _   _  ___ _ __   / _ \  */
+/* | |   / _` | | | |/ _ \ '__| | | | | */
+/* | |__| (_| | |_| |  __/ |    | |_| | */
+/* |_____\__,_|\__, |\___|_|     \___/  */
+/*             |___/                    */
     [0] = COLOR_LAYOUT( /* Layer 0 */
-    /*
-    LEFT SIDE
-
-    */
-        CFG_RGB_CHA_ISH, CFG_RGB_BLU_ISH, CFG_RGB_GRN_ISH, CFG_RGB_GRN_ISH, CFG_RGB_GRN_ISH, CFG_RGB_BEA_ISH, CFG_RGB_BEA_ISH,
-        CFG_RGB_WHT_ISH, CFG_RGB_GRN_ISH, CFG_RGB_GRN_ISH, CFG_RGB_SAL_ISH, CFG_RGB_BLU_ISH, CFG_RGB_BLU_ISH, CFG_RGB_CHA_ISH,
-        CFG_RGB_WHT_ISH, CFG_RGB_GRN_ISH, CFG_RGB_GRN_ISH, CFG_RGB_BLU_ISH, CFG_RGB_RED_ISH, CFG_RGB_GRN_ISH, CFG_RGB_WHT_ISH,
-        CFG_RGB_SAL_ISH, CFG_RGB_GRN_ISH, CFG_RGB_OFF    , CFG_RGB_SAL_ISH, CFG_RGB_SAL_ISH, CFG_RGB_GRN_ISH,
-        CFG_RGB_BEA_ISH, CFG_RGB_BLU_ISH, CFG_RGB_BLU_ISH, CFG_RGB_BEA_ISH, CFG_RGB_CHA_ISH,
-                                                                                                               CFG_RGB_BEA_ISH,
-                                                                            CFG_RGB_AQ2_ISH, CFG_RGB_BLU_ISH,  CFG_RGB_GRN_ISH,
-    /*
-    RIGHT SIDE
-    */
-        CFG_RGB_WHT_ISH, CFG_RGB_GRN_ISH, CFG_RGB_WHT_ISH, CFG_RGB_BLU_ISH, CFG_RGB_BEA_ISH, CFG_RGB_BLU_ISH, CFG_RGB_CHA_ISH,
-        CFG_RGB_WHT_ISH, CFG_RGB_GRN_ISH, CFG_RGB_SAL_ISH, CFG_RGB_WHT_ISH, CFG_RGB_CHA_ISH, CFG_RGB_GRN_ISH, CFG_RGB_SAL_ISH,
-        CFG_RGB_AQ2_ISH, CFG_RGB_GRN_ISH, CFG_RGB_BEA_ISH, CFG_RGB_BLU_ISH, CFG_RGB_CHA_ISH, CFG_RGB_GRN_ISH, CFG_RGB_BLU_ISH,
-                         CFG_RGB_GRN_ISH, CFG_RGB_CHA_ISH, CFG_RGB_CHA_ISH, CFG_RGB_CHA_ISH, CFG_RGB_GRN_ISH, CFG_RGB_WHT_ISH,
-                                          CFG_RGB_CHA_ISH, CFG_RGB_BLU_ISH, CFG_RGB_BLU_ISH, CFG_RGB_GRN_ISH, CFG_RGB_WHT_ISH,
-        CFG_RGB_BEA_ISH,
-        CFG_RGB_GRN_ISH, CFG_RGB_GRN_ISH, CFG_RGB_GRN_ISH
+        /* Left Hand */
+        CLR_CHART,  CLR_PRBLU,  CLR_SEGRN,  CLR_SEGRN,  CLR_SEGRN,  CLR_BEAVR,  CLR_BEAVR,                      CLR_WHT  ,  CLR_SEGRN,  CLR_WHT  ,  CLR_PRBLU,  CLR_BEAVR,  CLR_PRBLU,  CLR_CHART,
+        CLR_WHT  ,  CLR_SEGRN,  CLR_SEGRN,  CLR_CHART,  CLR_PRBLU,  CLR_PRBLU,  CLR_CHART,                      CLR_WHT  ,  CLR_SEGRN,  CLR_SALMN,  CLR_WHT  ,  CLR_CHART,  CLR_SEGRN,  CLR_SALMN,
+        CLR_WHT  ,  CLR_SEGRN,  CLR_SEGRN,  CLR_PRBLU,  CLR_RED  ,  CLR_SEGRN,  CLR_WHT  ,                      CLR_AQUA ,  CLR_SEGRN,  CLR_BEAVR,  CLR_PRBLU,  CLR_CHART,  CLR_SEGRN,  CLR_SALMN,
+        CLR_SALMN,  CLR_SEGRN,  CLR_OFF  ,  CLR_SALMN,  CLR_SALMN,  CLR_SEGRN,                                              CLR_SEGRN,  CLR_CHART,  CLR_CHART,  CLR_CHART,  CLR_SEGRN,  CLR_WHT  ,
+        CLR_BEAVR,  CLR_PRBLU,  CLR_PRBLU,  CLR_BEAVR,  CLR_CHART,              CLR_BEAVR,                      CLR_BEAVR,              CLR_CHART,  CLR_PRBLU,  CLR_PRBLU,  CLR_SEGRN,  CLR_WHT  ,
+                                                        CLR_AQUA ,  CLR_PRBLU,  CLR_SEGRN,                      CLR_SEGRN,  CLR_SEGRN,  CLR_BEAVR
     ),
-    [1] = COLOR_LAYOUT( /* Layer 1 */
-        CFG_RGB_OFF,     CFG_RGB_BEA_ISH, CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_BEA_ISH,
-        CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_BEA_ISH, CFG_RGB_OFF,     CFG_RGB_OFF,
-        CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_SAL_ISH, CFG_RGB_RED_ISH, CFG_RGB_OFF,     CFG_RGB_BEA_ISH,
-        CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_CHA_ISH, CFG_RGB_RED_ISH, CFG_RGB_OFF,
-        CFG_RGB_OFF,     CFG_RGB_BEA_ISH, CFG_RGB_BEA_ISH, CFG_RGB_OFF,     CFG_RGB_SPR_ISH,
-                                                                                     CFG_RGB_BEA_ISH,
-                                    CFG_RGB_RED_ISH, CFG_RGB_RED_ISH, CFG_RGB_RED_ISH,
-
-        CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_BEA_ISH, CFG_RGB_OFF,     CFG_RGB_SPR_ISH, CFG_RGB_OFF,     CFG_RGB_SPR_ISH,
-        CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_SAL_ISH, CFG_RGB_BEA_ISH, CFG_RGB_SPR_ISH, CFG_RGB_OFF,     CFG_RGB_OFF,
-        CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_BEA_ISH, CFG_RGB_BEA_ISH, CFG_RGB_SPR_ISH, CFG_RGB_OFF,     CFG_RGB_OFF,
-                     CFG_RGB_OFF,     CFG_RGB_CHA_ISH, CFG_RGB_OFF,     CFG_RGB_SPR_ISH, CFG_RGB_OFF,     CFG_RGB_CYA_ISH,
-                                  CFG_RGB_BEA_ISH, CFG_RGB_RED_ISH, CFG_RGB_RED_ISH, CFG_RGB_RED_ISH, CFG_RGB_OFF,
-                                                                                     CFG_RGB_OFF,
-                                                                  CFG_RGB_RED_ISH, CFG_RGB_RED_ISH, CFG_RGB_RED_ISH
+/*  _                            _  */
+/* | |    __ _ _   _  ___ _ __  / | */
+/* | |   / _` | | | |/ _ \ '__| | | */
+/* | |__| (_| | |_| |  __/ |    | | */
+/* |_____\__,_|\__, |\___|_|    |_| */
+/*             |___/                */
+    [1] = COLOR_LAYOUT(
+// +--------------+
+// | LAYER 1 LEFT |
+// +--------------+
+        CLR_OFF, CLR_BEAVR, CLR_OFF,   CLR_OFF,   CLR_OFF,   CLR_OFF,   CLR_BEAVR,
+        CLR_OFF, CLR_OFF,   CLR_OFF,   CLR_OFF,   CLR_BEAVR, CLR_OFF,   CLR_OFF,
+        CLR_OFF, CLR_OFF,   CLR_OFF,   CLR_SALMN, CLR_RED,   CLR_OFF,   CLR_BEAVR,
+        CLR_OFF, CLR_OFF,   CLR_OFF,   CLR_CHART, CLR_RED,   CLR_OFF,
+        CLR_OFF, CLR_BEAVR, CLR_BEAVR, CLR_OFF,   CLR_SPGRN,
+                                                                        CLR_BEAVR,
+                                    CLR_RED,   CLR_RED,   CLR_RED,
+// +---------------+
+// | LAYER 1 Right |
+// +---------------+
+        CLR_OFF, CLR_OFF,   CLR_BEAVR, CLR_OFF,   CLR_SPGRN, CLR_OFF,   CLR_SPGRN,
+        CLR_OFF, CLR_OFF,   CLR_SALMN, CLR_BEAVR, CLR_SPGRN, CLR_OFF,   CLR_OFF,
+        CLR_OFF, CLR_OFF,   CLR_BEAVR, CLR_BEAVR, CLR_SPGRN, CLR_OFF,   CLR_OFF,
+                     CLR_OFF,   CLR_CHART, CLR_OFF,   CLR_SPGRN, CLR_OFF,   CLR_CYPRO,
+                                  CLR_BEAVR, CLR_RED,   CLR_RED,   CLR_RED,   CLR_OFF,
+                                                                        CLR_OFF,
+                                                                  CLR_RED,   CLR_RED,   CLR_RED
     ),
-    [2] = COLOR_LAYOUT( /* Layer 2 */
-        CFG_RGB_OFF, CFG_RGB_OFF, CFG_RGB_OFF, CFG_RGB_OFF, CFG_RGB_OFF, CFG_RGB_OFF, CFG_RGB_OFF,
-        CFG_RGB_OFF, CFG_RGB_OFF, CFG_RGB_OFF, CFG_RGB_OFF, CFG_RGB_CHA_ISH, CFG_RGB_OFF, CFG_RGB_OFF,
-        CFG_RGB_CHA_ISH, CFG_RGB_OFF, CFG_RGB_OFF, CFG_RGB_CHA_ISH, CFG_RGB_CHA_ISH, CFG_RGB_CHA_ISH, CFG_RGB_OFF,
-        CFG_RGB_OFF, CFG_RGB_OFF, CFG_RGB_OFF, CFG_RGB_OFF, CFG_RGB_OFF, CFG_RGB_OFF,
-        CFG_RGB_RED_ISH, CFG_RGB_OFF, CFG_RGB_OFF, CFG_RGB_OFF, CFG_RGB_OFF,
-                                                                                     CFG_RGB_OFF,
-                                    CFG_RGB_OFF, CFG_RGB_OFF, CFG_RGB_OFF,
-
-        CFG_RGB_OFF, CFG_RGB_CHA_ISH, CFG_RGB_CHA_ISH, CFG_RGB_OFF, CFG_RGB_OFF, CFG_RGB_OFF, CFG_RGB_CHA_ISH,
-        CFG_RGB_OFF, CFG_RGB_CHA_ISH, CFG_RGB_CHA_ISH, CFG_RGB_CHA_ISH, CFG_RGB_OFF, CFG_RGB_OFF, CFG_RGB_OFF,
-        CFG_RGB_OFF, CFG_RGB_OFF, CFG_RGB_OFF, CFG_RGB_CHA_ISH, CFG_RGB_OFF, CFG_RGB_OFF, CFG_RGB_OFF,
-                     CFG_RGB_RED_ISH, CFG_RGB_OFF, CFG_RGB_OFF, CFG_RGB_OFF, CFG_RGB_RED_ISH, CFG_RGB_OFF,
-                                  CFG_RGB_CYA_ISH, CFG_RGB_CYA_ISH, CFG_RGB_OFF, CFG_RGB_OFF, CFG_RGB_RED_ISH,
-                                                                                     CFG_RGB_OFF,
-                                                                  CFG_RGB_OFF, CFG_RGB_OFF, CFG_RGB_OFF
+/*  _                            ____   */
+/* | |    __ _ _   _  ___ _ __  |___ \  */
+/* | |   / _` | | | |/ _ \ '__|   __) | */
+/* | |__| (_| | |_| |  __/ |     / __/  */
+/* |_____\__,_|\__, |\___|_|    |_____| */
+/*             |___/                    */
+    [2] = COLOR_LAYOUT(
+// +--------------+
+// | LAYER 2 LEFT |
+// +--------------+
+        CLR_OFF,   CLR_OFF,   CLR_OFF,   CLR_OFF,   CLR_OFF,   CLR_OFF,   CLR_OFF,
+        CLR_OFF,   CLR_OFF,   CLR_OFF,   CLR_OFF,   CLR_CHART, CLR_OFF,   CLR_OFF,
+        CLR_CHART, CLR_OFF,   CLR_OFF,   CLR_CHART, CLR_CHART, CLR_CHART, CLR_OFF,
+        CLR_OFF,   CLR_OFF,   CLR_OFF,   CLR_OFF,   CLR_OFF,   CLR_OFF,
+        CLR_RED,   CLR_OFF,   CLR_OFF,   CLR_OFF,   CLR_OFF,
+                                                                        CLR_OFF,
+                                    CLR_OFF,   CLR_OFF,   CLR_OFF,
+// +---------------+
+// | LAYER 2 Right |
+// +---------------+
+        CLR_OFF,   CLR_CHART, CLR_CHART, CLR_OFF,   CLR_OFF,   CLR_OFF,   CLR_CHART,
+        CLR_OFF,   CLR_CHART, CLR_CHART, CLR_CHART, CLR_OFF,   CLR_OFF,   CLR_OFF,
+        CLR_OFF,   CLR_OFF,   CLR_OFF,   CLR_CHART, CLR_OFF,   CLR_OFF,   CLR_OFF,
+                     CLR_RED,   CLR_OFF,   CLR_OFF,   CLR_OFF,   CLR_RED,   CLR_OFF,
+                                  CLR_CYPRO, CLR_CYPRO, CLR_OFF,   CLR_OFF,   CLR_RED,
+                                                                        CLR_OFF,
+                                                                  CLR_OFF,   CLR_OFF,   CLR_OFF
     ),
-    [3] = COLOR_LAYOUT( /* Layer 3 */
-        CFG_RGB_PRBLU,   CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_BEAVR,   CFG_RGB_OFF,
-        CFG_RGB_OFF,     CFG_RGB_BEAVR,   CFG_RGB_CHART,   CFG_RGB_CHART,   CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_BEAVR,
-        CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_BEAVR,   CFG_RGB_SALMN,   CFG_RGB_SALMN,   CFG_RGB_SALMN,   CFG_RGB_OFF,
-        CFG_RGB_SALMN,   CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_BEAVR,   CFG_RGB_OFF,     CFG_RGB_OFF,
-        CFG_RGB_OFF,     CFG_RGB_BEAVR,   CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_OFF,
-                                                                                     CFG_RGB_BEAVR,
-                                    CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_OFF,
-
-        CFG_RGB_OFF,     CFG_RGB_SALMN,   CFG_RGB_SALMN,   CFG_RGB_OFF,     CFG_RGB_BEAVR,   CFG_RGB_SALMN,   CFG_RGB_SALMN,
-        CFG_RGB_OFF,     CFG_RGB_SALMN,   CFG_RGB_SALMN,   CFG_RGB_SALMN,   CFG_RGB_OFF,     CFG_RGB_BEAVR,   CFG_RGB_CHART,
-        CFG_RGB_BEAVR,   CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_BEAVR,   CFG_RGB_SALMN,
-                     CFG_RGB_OFF,     CFG_RGB_BEAVR,   CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_OFF,
-                                  CFG_RGB_BEAVR,   CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_SPGRN,   CFG_RGB_BEAVR,
-                                                                                     CFG_RGB_OFF,
-                                                                  CFG_RGB_OFF,     CFG_RGB_BEAVR,   CFG_RGB_OFF
+/*  _                            _____  */
+/* | |    __ _ _   _  ___ _ __  |___ /  */
+/* | |   / _` | | | |/ _ \ '__|   |_ \  */
+/* | |__| (_| | |_| |  __/ |     ___) | */
+/* |_____\__,_|\__, |\___|_|    |____/  */
+/*             |___/                    */
+    [3] = COLOR_LAYOUT(
+// +--------------+
+// | LAYER 3 LEFT |
+// +--------------+
+        CLR_PRBLU, CLR_OFF,   CLR_OFF,   CLR_OFF,   CLR_OFF,   CLR_BEAVR, CLR_OFF,
+        CLR_OFF,   CLR_BEAVR, CLR_CHART, CLR_CHART, CLR_OFF,   CLR_OFF,   CLR_BEAVR,
+        CLR_OFF,   CLR_OFF,   CLR_BEAVR, CLR_SALMN, CLR_SALMN, CLR_SALMN, CLR_OFF,
+        CLR_SALMN, CLR_OFF,   CLR_OFF,   CLR_BEAVR, CLR_OFF,   CLR_OFF,
+        CLR_OFF,   CLR_BEAVR, CLR_OFF,   CLR_OFF,   CLR_OFF,
+                                                                        CLR_BEAVR,
+                                    CLR_OFF,   CLR_OFF,   CLR_OFF,
+// +---------------+
+// | LAYER 3 Right |
+// +---------------+
+        CLR_OFF,   CLR_SALMN, CLR_SALMN, CLR_OFF,   CLR_BEAVR, CLR_SALMN, CLR_SALMN,
+        CLR_OFF,   CLR_SALMN, CLR_SALMN, CLR_SALMN, CLR_OFF,   CLR_BEAVR, CLR_CHART,
+        CLR_BEAVR, CLR_OFF,   CLR_OFF,   CLR_OFF,   CLR_OFF,   CLR_BEAVR, CLR_SALMN,
+                     CLR_OFF,   CLR_BEAVR, CLR_OFF,   CLR_OFF,   CLR_OFF,   CLR_OFF,
+                                  CLR_BEAVR, CLR_OFF,   CLR_OFF,   CLR_SPGRN, CLR_BEAVR,
+                                                                        CLR_OFF,
+                                                                  CLR_OFF,   CLR_BEAVR, CLR_OFF
     ),
-    [4] = COLOR_LAYOUT( /* Layer 4 */
-        CFG_RGB_WHT,     CFG_RGB_CHART,   CFG_RGB_OFF,     CFG_RGB_CHART,   CFG_RGB_CHART,   CFG_RGB_WHT,     CFG_RGB_WHT,
-        CFG_RGB_OFF,     CFG_RGB_WHT,     CFG_RGB_PRBLU,   CFG_RGB_SALMN,   CFG_RGB_PRBLU,   CFG_RGB_BEAVR,   CFG_RGB_WHT,
-        CFG_RGB_WHT,     CFG_RGB_CHART,   CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_CHART,   CFG_RGB_CHART,   CFG_RGB_CHART,
-        CFG_RGB_WHT,     CFG_RGB_WHT,     CFG_RGB_WHT,     CFG_RGB_WHT,     CFG_RGB_WHT,     CFG_RGB_WHT,
-        CFG_RGB_WHT,     CFG_RGB_WHT,     CFG_RGB_OFF,     CFG_RGB_WHT,     CFG_RGB_WHT,
-                                                                                     CFG_RGB_OFF,
-                                    CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_OFF,
-
-        CFG_RGB_WHT,     CFG_RGB_SALMN,   CFG_RGB_WHT,     CFG_RGB_OFF,     CFG_RGB_WHT,     CFG_RGB_SALMN,   CFG_RGB_SALMN,
-        CFG_RGB_WHT,     CFG_RGB_WHT,     CFG_RGB_PRBLU,   CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_WHT,     CFG_RGB_WHT,
-        CFG_RGB_WHT,     CFG_RGB_CHART,   CFG_RGB_PRBLU,   CFG_RGB_PRBLU,   CFG_RGB_WHT,     CFG_RGB_CHART,   CFG_RGB_OFF,
-                     CFG_RGB_WHT,     CFG_RGB_WHT,     CFG_RGB_WHT,     CFG_RGB_WHT,     CFG_RGB_WHT,     CFG_RGB_WHT,
-                                  CFG_RGB_PRBLU,   CFG_RGB_WHT,     CFG_RGB_WHT,     CFG_RGB_OFF,     CFG_RGB_PRBLU,
-                                                                                     CFG_RGB_OFF,
-                                                                  CFG_RGB_OFF,     CFG_RGB_OFF,     CFG_RGB_OFF
+/*  _                            _  _    */
+/* | |    __ _ _   _  ___ _ __  | || |   */
+/* | |   / _` | | | |/ _ \ '__| | || |_  */
+/* | |__| (_| | |_| |  __/ |    |__   _| */
+/* |_____\__,_|\__, |\___|_|       |_|   */
+/*             |___/                     */
+    [4] = COLOR_LAYOUT(
+// +--------------+
+// | LAYER 4 LEFT |
+// +--------------+
+        CLR_WHT, CLR_CHART, CLR_OFF,   CLR_CHART, CLR_CHART, CLR_WHT,   CLR_WHT,
+        CLR_OFF, CLR_WHT,   CLR_PRBLU, CLR_SALMN, CLR_PRBLU, CLR_BEAVR, CLR_WHT,
+        CLR_WHT, CLR_CHART, CLR_OFF,   CLR_OFF,   CLR_CHART, CLR_CHART, CLR_CHART,
+        CLR_WHT, CLR_WHT,   CLR_WHT,   CLR_WHT,   CLR_WHT,   CLR_WHT,
+        CLR_WHT, CLR_WHT,   CLR_OFF,   CLR_WHT,   CLR_WHT,
+                                                                        CLR_OFF,
+                                    CLR_OFF,   CLR_OFF,   CLR_OFF,
+// +---------------+
+// | LAYER 4 Right |
+// +---------------+
+        CLR_WHT, CLR_SALMN, CLR_WHT,   CLR_OFF,   CLR_WHT,   CLR_SALMN, CLR_SALMN,
+        CLR_WHT, CLR_WHT,   CLR_PRBLU, CLR_OFF,   CLR_OFF,   CLR_WHT,   CLR_WHT,
+        CLR_WHT, CLR_CHART, CLR_PRBLU, CLR_PRBLU, CLR_WHT,   CLR_CHART, CLR_OFF,
+                     CLR_WHT,   CLR_WHT,   CLR_WHT,   CLR_WHT,   CLR_WHT,   CLR_WHT,
+                                  CLR_PRBLU, CLR_WHT,   CLR_WHT,   CLR_OFF,   CLR_PRBLU,
+                                                                        CLR_OFF,
+                                                                  CLR_OFF,   CLR_OFF,   CLR_OFF
     ),
 };
 
-// Updated function to use the layout-based map and LED index mapping (using RGB)
+// Updated function to use the layout-based map and LED index mapping (now expecting HSV)
 void set_layer_color(int layer) {
     // Ensure g_led_config is accessible (defined in keyboard's .c file)
     extern led_config_t g_led_config;
@@ -147,19 +184,27 @@ void set_layer_color(int layer) {
                 continue;
             }
 
-            // Get the R, G, B values directly from the layout map
-            uint8_t r = pgm_read_byte(&ledmap_layout[layer][i][j][0]);
-            uint8_t g = pgm_read_byte(&ledmap_layout[layer][i][j][1]);
-            uint8_t b = pgm_read_byte(&ledmap_layout[layer][i][j][2]);
+            // Get the H, S, V values directly from the layout map
+            HSV hsv = {
+                .h = pgm_read_byte(&ledmap_layout[layer][i][j][0]),
+                .s = pgm_read_byte(&ledmap_layout[layer][i][j][1]),
+                .v = pgm_read_byte(&ledmap_layout[layer][i][j][2]),
+            };
 
-            // Apply brightness
-            float brightness_factor = (float)rgb_matrix_config.hsv.v / UINT8_MAX;
-            r = (uint8_t)(r * brightness_factor);
-            g = (uint8_t)(g * brightness_factor);
-            b = (uint8_t)(b * brightness_factor);
-
-            // Set the color
-            rgb_matrix_set_color(led_index, r, g, b);
+            // Set the color (applying brightness)
+            if (!hsv.h && !hsv.s && !hsv.v) {
+                // If color is {0,0,0}, explicitly turn LED off
+                rgb_matrix_set_color(led_index, 0, 0, 0);
+            } else {
+                // Convert HSV to RGB *after* retrieving from map
+                RGB rgb = hsv_to_rgb(hsv);
+                // Apply brightness factor from global config
+                float brightness_factor = (float)rgb_matrix_config.hsv.v / UINT8_MAX;
+                rgb_matrix_set_color(led_index,
+                                     (uint8_t)(rgb.r * brightness_factor),
+                                     (uint8_t)(rgb.g * brightness_factor),
+                                     (uint8_t)(rgb.b * brightness_factor));
+            }
         }
     }
 }
--- rgb_config/rgb_config.h
+++ rgb_config/rgb_config.h
@@ -8,7 +8,7 @@ void set_layer_color(int layer);
 
 // Macro to visually define LED colors based on physical layout (72 keys)
 // Maps the 72 visual arguments to the correct [MATRIX_ROWS(12)][MATRIX_COLS(7)] structure
-// The macro itself expands into RGB triplets, which are expected by set_layer_color.
+// The macro itself expands into HSV triplets {H, S, V}, which are expected by set_layer_color.
 #define COLOR_LAYOUT( \
     c00, c01, c02, c03, c04, c05, c06,   c60, c61, c62, c63, c64, c65, c66, \
     c10, c11, c12, c13, c14, c15, c16,   c70, c71, c72, c73, c74, c75, c76, \
@@ -16,7 +16,7 @@ void set_layer_color(int layer);
     c30, c31, c32, c33, c34, c35,             c91, c92, c93, c94, c95, c96, \
     c40, c41, c42, c43, c44,      c53,   kb3,      ca2, ca3, ca4, ca5, ca6, \
                         c50, c51, c52,   kb4, kb5, kb6                       \
-) { /* Expands to [12][7][3] array storing {R,G,B} */ \
+) { /* Expands to [12][7][3] array storing {H,S,V} */ \
     /* ROW 0 */ { c00, c01, c02, c03, c04, c05, c06 }, \
     /* ROW 1 */ { c10, c11, c12, c13, c14, c15, c16 }, \
     /* ROW 2 */ { c20, c21, c22, c23, c24, c25, c26 }, \
